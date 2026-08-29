e506492700d1ed835e79b9360f45f67d1b45534a
amrudinbalic <balicamrudin@gmail.com>
2026-08-29 08:50:37 +0200

    sub-dev

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,6 +3,6 @@ import { Request, Response } from 'express';
 export const getHome = (req: Request, res: Response) => {
   res.render('index', {
     title: process.env.APP_NAME,
-    message: `${process.env.APP_NAME} is a small booking platform lent towards travelers on their vacations.`
+    message: `${process.env.APP_NAME} is a small booking platform lent towards travelers.`
   });
 };
